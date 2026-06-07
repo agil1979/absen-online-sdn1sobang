83d3c3375b9e0e98a72d7219571c728b5f30aae0
agil1979 <ahendrawan626@gmail.com>
2026-06-07 09:51:06 +0700

    Update WEB_APP_URL and retain functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzJ-BHo7nOTVO7iEFhGzVRwkAY5ybixq8NjFv4jOdx4nwZe0TyY8Se2qrP1j5yGCNZF/exec"; // PASTIKAN SUDAH DIGANTI!
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycby3XL-9qVn94DdBCsBonTde15tMDNedfSeZYneGwXaIwHnNxg3nrH0i9xG86bgQjiQg/exec"; // PASTIKAN SUDAH DIGANTI!
 const OFFICE_LAT = -6.628209010488044; 
 const OFFICE_LNG = 106.29402842818563;
 const MAX_DISTANCE = 100;
